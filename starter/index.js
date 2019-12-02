a02bf2000e2445258f17ba3b9d8764929bcde6f0
Austin Vickers <austin.vickers@hexagonsi.com>
2019-12-02 10:52:02 -0600

    add parcel to project

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,6 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('div', null, 'React element!');
+const element = <div> React element with JSX </div>;
+
 ReactDOM.render(element, document.getElementById('root'));

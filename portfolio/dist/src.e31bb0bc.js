3fd316f92b286b7b5998e5a99a9b4dbe599fb6f2
Austin Vickers <austin.vickers@hexagonsi.com>
2019-12-04 11:12:31 -0600

    add multiple jokes button

--- portfolio/dist/src.e31bb0bc.js
+++ portfolio/dist/src.e31bb0bc.js
@@ -24437,7 +24437,18 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// Display a fetched joke from the api
+var Joke = function Joke(props) {
+  var _props$joke = props.joke,
+      setup = _props$joke.setup,
+      punchline = _props$joke.punchline;
+  return _react.default.createElement("p", {
+    style: {
+      margin: 20
+    }
+  }, setup, " ", _react.default.createElement("em", null, punchline));
+}; // Display a fetched joke from the api
+
+
 var Jokes =
 /*#__PURE__*/
 function (_Component) {
@@ -24457,7 +24468,19 @@ function (_Component) {
     _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Jokes)).call.apply(_getPrototypeOf2, [this].concat(args)));
 
     _defineProperty(_assertThisInitialized(_this), "state", {
-      joke: {}
+      joke: {},
+      jokes: []
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "fetchJokes", function () {
+      // Fetch the joke asynchronously
+      fetch('https://official-joke-api.appspot.com/random_ten').then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        return _this.setState({
+          jokes: json
+        });
+      });
     });
 
     return _this;
@@ -24480,14 +24503,20 @@ function (_Component) {
   }, {
     key: "render",
     value: function render() {
-      var _this$state$joke = this.state.joke,
-          setup = _this$state$joke.setup,
-          punchline = _this$state$joke.punchline;
       return _react.default.createElement("div", {
         style: {
           marginBottom: 200
         }
-      }, _react.default.createElement("h2", null, "Highlighted Joke"), _react.default.createElement("p", null, setup, " ", _react.default.createElement("em", null, punchline)));
+      }, _react.default.createElement("h2", null, "Highlighted Joke"), _react.default.createElement(Joke, {
+        joke: this.state.joke
+      }), _react.default.createElement("button", {
+        onClick: this.fetchJokes
+      }, "See more jokes"), this.state.jokes.map(function (joke) {
+        return _react.default.createElement(Joke, {
+          key: joke.id,
+          joke: joke
+        });
+      }), _react.default.createElement("hr", null));
     }
   }]);
 
@@ -24653,7 +24682,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59283" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59025" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- portfolio/src/Components/Jokes.js
+++ portfolio/src/Components/Jokes.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 
+const Joke = (props) => {
+    const { setup, punchline } = props.joke;
+    return <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>;
+}
+
+
 // Display a fetched joke from the api
 class Jokes extends Component{
-    state = { joke: {}};
+    state = { joke: {}, jokes: []};
 
     componentDidMount(){
         // Fetch the joke asynchronously
         fetch('https://official-joke-api.appspot.com/random_joke')
-            .then((response) => response.json())
-            .then(json => this.setState({joke: json}));
+        .then((response) => response.json())
+        .then(json => this.setState({joke: json}));
+    }
+
+    fetchJokes = () => {
+         // Fetch the joke asynchronously
+         fetch('https://official-joke-api.appspot.com/random_ten')
+         .then((response) => response.json())
+         .then(json => this.setState({jokes: json}));
     }
 
     render(){
-        const { setup, punchline } = this.state.joke;
-
         return (
             <div style={{marginBottom: 200}}>
                 <h2>Highlighted Joke</h2>
-                <p>{setup} <em>{punchline}</em></p>
+                <Joke joke={this.state.joke}/>
+                <button onClick={this.fetchJokes}>See more jokes</button>
+                { this.state.jokes.map(joke => <Joke key={joke.id} joke={joke}/>) } 
+            <hr></hr>
             </div>
         )
     }
